550ea063a99bc1d801635f08f708e060b3473c0e
Koen Castermans <castermanskoen@msn.com>
2017-05-05 20:07:00 +0200

    project setup

--- main.js
+++ main.js
@@ -1,45 +1,14 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const {app} = require('electron')
+const mainWindow = require('./mainWindow')
 
-const path = require('path')
-const url = require('url')
+// Enable Electron-reload
+require('electron-reload')(__dirname)
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-
-function createWindow () { 
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1200, height: 800})
-
-  // and load the index.html of the app.
-  console.log('loading index.html into mainWindow')  
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-  console.log('mainWindow closed')    
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
-}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', mainWindow.createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -54,7 +23,7 @@ app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    mainWindow.createWindow()
   }
 })
 
